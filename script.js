5bf4efaf582a1faa1fa940021e3d6e7a4ca47fea
kRehman007 <168665220+kRehman007@users.noreply.github.com>
2024-07-04 23:06:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,49 @@
 const articleDiv=document.querySelector('.articles');
 const inputBox=document.getElementById('inputBox');
-const api="d90ae4cc1df1430d899f9147b603631f";
-const url="https://newsapi.org/v2/everything?q="
 
 window.addEventListener('load',()=> fetchNews('India'));
 
-async function fetchNews(query){
-    const response=await fetch(`${url}${query}&apiKey=${api}`);
-    const data=await response.json();
-    const result=data.articles;
-    console.log(result);
-
-    articleDiv.innerHTML='';
-
-    result.map((res)=>{
-        if(!res.urlToImage){return}
-
-        const Div=document.createElement('div');
+async function fetchNews(cat){
+    const url = 'https://google-api31.p.rapidapi.com/';
+    const options = {
+        method: 'POST',
+        headers: {
+            'x-rapidapi-key': '31f7225694msh179cc057e09527bp15f5a3jsnd2462a592ca8',
+            'x-rapidapi-host': 'google-api31.p.rapidapi.com',
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify({
+            text: cat,
+            region: 'wt-wt',
+            max_results: 25
+        })
+    };
+    
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+          console.log(result);
         
-        const image=document.createElement('img');
-        const imageLink=document.createElement('a');
-        const title=document.createElement('h1');
-        const date=document.createElement('span');
-        const desc=document.createElement('p');
+        articleDiv.innerHTML='';
 
-        const newDate= new Date(res.publishedAt).toLocaleString('en-US',{
-            timeZone:'Asia/Jakarta'
-        });
-
-        image.src=res.urlToImage;
+        result.news.map((res)=>{
+            if(!res.image){return}
+    
+            const Div=document.createElement('div');
+            
+            const image=document.createElement('img');
+            const imageLink=document.createElement('a');
+            const title=document.createElement('h1');
+            const date=document.createElement('span');
+            const desc=document.createElement('p');
+    
+            const newDate= new Date(res.date).toLocaleString('en-US',{
+                timeZone:'Asia/Jakarta'
+            });
+            image.src=res.image;
         title.textContent=res.title;
-        date.textContent=`${res.source.name} . ${newDate}`;
-        desc.textContent=res.description;
+        date.textContent=`${res.source} . ${newDate}`;
+        desc.textContent=res.body;
         imageLink.href=res.url;
         imageLink.appendChild(image);
         Div.appendChild(imageLink);
@@ -43,9 +55,13 @@ async function fetchNews(query){
         imageLink.target="_blank";
    
     });
-}
 
-let list=document.querySelectorAll('li');
+    } catch (error) {
+        console.error(error);
+    }
+    }
+
+    let list=document.querySelectorAll('li');
 
 function LoadingScreen(id){
     for(li of list){
